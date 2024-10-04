50a7f8b5298c870888e59d2bc07e9edaf182d51e
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-10-04 21:01:48 +0000

    final commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@
 #
 # Table name: comments
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -18,16 +18,16 @@ class Comment < ApplicationRecord
   ## Direct associations
 
   # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
-
+  belongs_to(:commenter, class_name: "User", foreign_key: "author_id")
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
+  #def commenter
+  #  my_author_id = self.author_id
 
-  def commenter
-    my_author_id = self.author_id
+  #  matching_users = User.where({ :id => my_author_id })
 
-    matching_users = User.where({ :id => my_author_id })
+  #  the_user = matching_users.at(0)
 
-    the_user = matching_users.at(0)
-
-    return the_user
-  end
+  #  return the_user
+  #end
 end
